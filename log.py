ccd1a5ddb2a11ed35064018e6631aac187423740
cjj0596 <cjj0596@175game.com>
2016-02-01 11:18:58 +0800

    test in Linux

--- log.py
+++ log.py
@@ -2,32 +2,70 @@
 import re
 import time
 import linecache
+from collections import Counter
 
-f=open("/media/sf_virtual_share/ab.log","r")
+f=open("test_log.log",'r')
 first_time=re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',f.readline()).group(0)
 ftime=time.mktime(time.strptime(first_time,'%Y-%m-%d %H:%M:%S'))
-ftime+=10*60
-x = time.localtime(ftime)
-print time.strftime('%Y-%m-%d %H:%M:%S',x)
-end=linecache.getline("/media/sf_virtual_share/ab.log",len(open("/media/sf_virtual_share/ab.log",'r').readlines()))
+
+end=linecache.getline("test_log.log",len(open("test_log.log",'r').readlines()))
 end_time=re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',end).group(0)
 etime=time.mktime(time.strptime(end_time,'%Y-%m-%d %H:%M:%S'))
 
-list=[]
+ctime=ftime+10*60
+f.close()
+
+f=open("test_log.log","r")
+i = 0
+list1=[]
+print "------------------------------------------------"
 for line in f:
-    rtime=re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',line).group(0)
-    ctime=ftime=time.mktime(time.strptime(rtime,'%Y-%m-%d %H:%M:%S'))
-    if ctime <= ftime :
-         command=re.search(r'\t([\d\w\W]*)\s\s',line).group(0)
-         list.append(command)
-    else:
-         break;
-
-
-print list
-
-
-
-
-
+    try:
+       atime=re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',line).group(0)
+       atime2=time.mktime(time.strptime(atime,'%Y-%m-%d %H:%M:%S'))
+       if (atime2 <= ctime):
+	   if (i == 0):
+	       print "data from " + atime + " to " + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(ctime))
+           i += 1
+           command=re.search(r'\t([\d\w\W]*)\.[\w\s]',line).group(0)
+           if (command != ''):
+	      list1.append(command)
+       elif (atime2 > ctime and (ctime+10*60) <= etime):
+	   result=dict(Counter(list1))
+	   for k in result:
+               print result[k],k
+           print "------------------------------------------------"
+           i=0
+	   list1=[]
+	   result={}
+           if (atime2 > (ctime+10*60)):
+               ctime+=10*60*2
+           else:
+               ctime+=10*60
+           i += 1
+           print "data from " + atime + " to " + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(ctime))
+	   command=re.search(r'\t([\d\w\W]*)\.[\w\s]',line).group(0)
+           if (command != ''):
+	      list1.append(command)
+       else:
+	   result=dict(Counter(list1))
+           for k in result:
+	       print result[k],k
+           print "------------------------------------------------"     
+           i=0
+           list1=[]
+	   result={}
+           i +=1
+           ctime=etime
+	   print "data from " + atime + " to " + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(ctime))
+           command=re.search(r'\t([\d\w\W]*)\.[\w\s]',line).group(0)
+           if (command != ''):
+	      list1.append(command)
+    except AttributeError:
+           continue
+result=dict(Counter(list1))
+for k in result:
+    print result[k],k
+print "------------------------------------------------"           
+print "Done!"
 f.close()
--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-atime=$(date +%s -d "`head -1 /media/sf_virtual_share/test_log.log|cut -f1`")
-ltime=$(date +%s -d "`tail -1 /media/sf_virtual_share/test_log.log|cut -f1`")
+atime=$(date +%s -d "`head -1 test_log.log|cut -f1`")
+ltime=$(date +%s -d "`tail -1 test_log.log|cut -f1`")
 ctime=$((${atime}+10*60))
 
 while(( ${ctime} <= ${ltime} ))
@@ -11,7 +11,7 @@ do
     echo -e "--------------------------------------------------------"
     echo "data from ${btime} to ${etime}"
    # sed -n '/'"${btime}"'/,/'"${etime}"'/p' /media/sf_virtual_share/test_log.log|awk '{a[$3]++}END{for(i in a){if(i!="")print i,a[i]}}'|column -t
-    awk '/'"${btime}"'/,/'"${etime}"'/{a[$3]++}END{for(i in a){print i,a[i] | "sort -k 1"}}' /media/sf_virtual_share/test_log.log|column -t
+    awk '/'"${btime}"'/,/'"${etime}"'/{a[$3]++}END{for(i in a){print i,a[i] | "sort -k 1"}}' test_log.log|column -t
     atime=${ctime}
     ctime=$(($ctime+10*60))
 done
@@ -21,8 +21,6 @@ ftime=`date -d "1970-01-01 UTC ${ltime} seconds" "+%F %T"`
 ztime=`date -d "1970-01-01 UTC ${atime} seconds" "+%F %T"`
 echo "data from ${ztime} to ${ftime}"
    # sed -n '/'"${ztime}"'/,/'"${ftime}"'/p' /media/sf_virtual_share/test_log.log|awk '{a[$3]++}END{for(i in a){if(i!="")print i,a[i]}}'|column -t
-    awk '/'"${ztime}"'/,/'"${ftime}"'/{a[$3]++}END{for(i in a){print i,a[i] | "sort -k 1"}}' /media/sf_virtual_share/test_log.log|column -t 
+    awk '/'"${ztime}"'/,/'"${ftime}"'/{a[$3]++}END{for(i in a){print i,a[i] | "sort -k 1"}}' test_log.log|column -t 
     echo -e "--------------------------------------------------------\n"
 echo "Done!"
-
-
